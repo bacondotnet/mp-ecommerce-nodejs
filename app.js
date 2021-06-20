a41618fe5d7042f44973341fdc538674d659f918
Matias Mieres <matiasmieres64@gmail.com>
2021-06-20 17:52:39 -0300

    uno

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+var express = require("express");
+var exphbs = require("express-handlebars");
+var port = process.env.PORT || 3000;
 
-var app = express();
- 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
-
-app.get('/', function (req, res) {
-    res.render('home');
+// SDK de Mercado Pago
+const mercadopago = require("mercadopago");
+// Agrega credenciales
+mercadopago.configure({
+  access_token:
+    "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+// Crea un objeto de preferencia
+let preference = {
+  items: [
+    {
+      title: "Mi producto",
+      unit_price: 100,
+      quantity: 1,
+    },
+  ],
+  back_urls: {
+    success: "http://localhost:8080/feedback",
+    failure: "http://localhost:8080/feedback",
+    pending: "http://localhost:8080/feedback",
+  },
+  auto_return: "approved",
+};
+
+mercadopago.preferences
+  .create(preference)
+  .then(function (response) {
+    // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+    global.id = response.body.id;
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+var app = express();
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use(express.static("assets"));
+
+app.use("/assets", express.static(__dirname + "/assets"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/detail", function (req, res) {
+  res.render("detail", req.query);
 });
 
 app.listen(port);
