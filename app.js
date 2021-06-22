0bff3924b8b1d9a67a222779f7712fe07691cd37
Matias Mieres <matiasmieres64@gmail.com>
2021-06-21 21:26:51 -0300

    siuuu

--- app.js
+++ app.js
@@ -8,36 +8,12 @@ const mercadopago = require("mercadopago");
 mercadopago.configure({
   access_token:
     "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
+  integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
 });
 
-// Crea un objeto de preferencia
-let preference = {
-  items: [
-    {
-      title: "Mi producto",
-      unit_price: 100,
-      quantity: 1,
-    },
-  ],
-  back_urls: {
-    success: "http://localhost:8080/feedback",
-    failure: "http://localhost:8080/feedback",
-    pending: "http://localhost:8080/feedback",
-  },
-  auto_return: "approved",
-};
-
-mercadopago.preferences
-  .create(preference)
-  .then(function (response) {
-    // Este valor reemplazará el string "<%= global.id %>" en tu HTML
-    global.id = response.body.id;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
 var app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
@@ -52,6 +28,83 @@ app.get("/", function (req, res) {
 
 app.get("/detail", function (req, res) {
   res.render("detail", req.query);
+});
+
+app.get("/feedback", function (req, res) {
+  res.render("feedback", req.query);
+});
+
+app.post("/create_preference", (req, res) => {
+  let preference = {
+    items: [
+      {
+        id: 1234,
+        title: req.body.title,
+        description: "Dispositivo móvil de Tienda e-commerce",
+        picture_url: req.body.picture_url,
+        quantity: Number(req.body.quantity),
+        unit_price: Number(req.body.unit_price),
+      },
+    ],
+    external_reference: "matiasmieres64@gmail.com",
+    payer: {
+      name: "Lalo",
+      surname: "Landa",
+      email: "test_user_63274575@testuser.com",
+      phone: {
+        area_code: "11",
+        number: 22223333,
+      },
+      address: {
+        street_name: "Falsa",
+        street_number: 123,
+        zip_code: "1111",
+      },
+    },
+    back_urls: {
+      success:
+        "https://bacondotnet-mp-commerce-nodej.herokuapp.com/feedback?feedback=success",
+      failure:
+        "https://bacondotnet-mp-commerce-nodej.herokuapp.com/feedback?feedback=failure",
+      pending:
+        "https://bacondotnet-mp-commerce-nodej.herokuapp.com/feedback?feedback=pending",
+    },
+    auto_return: "approved",
+    payment_methods: {
+      excluded_payment_methods: [
+        {
+          id: "amex",
+        },
+      ],
+      excluded_payment_types: [
+        {
+          id: "atm",
+        },
+      ],
+      installments: 6,
+    },
+    notification_url:
+      "https://bacondotnet-mp-commerce-nodej.herokuapp.com/notifications",
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.json(response.body);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.post("/notifications", (req, res) => {
+  console.log(req.body);
+
+  if (req.body.action == "payment.created") {
+    return res.status(201).json({});
+  } else {
+    return res.status(200).json({});
+  }
 });
 
 app.listen(port);
